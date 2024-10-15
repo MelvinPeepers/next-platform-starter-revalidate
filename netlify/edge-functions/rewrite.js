4ac024d0e459372b3bfc12d6c5c6300586c2dfd4
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-10-15 11:17:53 -0500

    updated Edge Function

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,5 +1,16 @@
 const rewrite = async (request, context) => {
-    const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
+    // const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
+    // return new URL(path, request.url);
+    let path;
+
+    if (context.geo?.country?.code === 'AU') {
+        path = '/edge/australia';
+    } else if (context.geo?.country?.code === 'US') {
+        path = '/edge/usa';
+    } else {
+        path = '/edge/other';
+    }
+
     return new URL(path, request.url);
 };
 
